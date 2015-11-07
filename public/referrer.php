699ebd3db6021c3a54caf2532a74ac145f3aa1e3
Rick Measham <rick@measham.id.au>
2015-11-07 12:04:24 +1100

    Positive confirmation on no referrer

--- public/referrer.php
+++ public/referrer.php
@@ -2,9 +2,13 @@
 <head>
 	<title>squawk.cc - referrer display</title>
 </head>
-<body>
+<body style="background-color:white">
 	<h2>Referrer</h2>
-	<p>You were referred to this page by:</p>
-	<pre><?php echo $_SERVER['HTTP_REFERER']; ?></pre>
+	<?php if( $_SERVER['HTTP_REFERER'] ) :?>
+		<p>You were referred to this page by:</p>
+		<pre><?php echo $_SERVER['HTTP_REFERER']; ?></pre>
+	<?php else : ?>
+		<p>There was no referrer sent from your browser.</p>
+	<?php endif; ?>
 </body>
 </html>
